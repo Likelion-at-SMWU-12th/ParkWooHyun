4e02c79a78822b71553f5ef04e8eca6f57579bde
박우현 <parkwh0122@gmail.com>
2024-03-19 20:33:38 +0900

    js 추가

---
+++ week02/woohyun.js
@@ -0,0 +1,10 @@
+const mainbox=document.getElementById("mainbox");
+function darkmode() {
+    mainbox.style.backgroundColor = '#262626';
+    mainbox.style.color = 'white';
+}
+function lightmode() {
+    mainbox.style.backgroundColor = 'white';
+    mainbox.style.color = 'black';
+    h2.style.backgroundColor = 'cornsilk';
+}
